593a995d2a75d6eb1f22fff48a65b5a5c3536d92
TrifanIaroslav <iaroslav.trifan@isa.utm.md>
2024-03-09 11:57:11 +0200

    catalinloh

---
+++ eUseControl/eUseControl.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace eUseControl.Web
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            /*
+            // Bootstrap style
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            //Bootstrap
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap/js").Include("~/Scripts/bootstrap.min.js"));
+
+            //Bootstrap style
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include("~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+            */
+        }
+    }
+}
