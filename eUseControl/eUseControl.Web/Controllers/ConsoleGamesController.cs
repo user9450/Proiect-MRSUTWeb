d683006c6d5653609ae236a3df0544e8a5a789e5
Procopiuc Alina <commandline.flow@gmail.com>
2024-06-05 21:35:39 +0300

    ConsoleGames page

---
+++ eUseControl/eUseControl.Web/Controllers/ConsoleGamesController.cs
@@ -0,0 +1,30 @@
+﻿using eUseControl.BusinessLogic.DBModel;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class ConsoleGamesController : Controller
+    {
+        private readonly ISession _session;
+        private readonly OrderContext _context;
+
+        public ConsoleGamesController()
+        {
+            var bl = new BussinesLogic();
+            _session = bl.GetSessionBL();
+            _context = new OrderContext();
+        }
+        // GET: ConsoleGames
+        public ActionResult ConsoleGames()
+        {
+            var products = _context.Products.ToList(); // Get products from DB
+            return View(products);
+        }
+    }
+}
